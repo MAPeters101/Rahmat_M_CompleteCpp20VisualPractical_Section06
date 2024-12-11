675522194cb8f8e9ef772865e5e4f60208f26b57
Mark Peters <mapeters101@earthlink.net>
2024-12-11 15:29:43 -0500

    Work on lesson064.

---
+++ lesson064.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	int Numbers[5];
+	Numbers[0] = 9;
+
+	cout << "Number at 1 is: " << Numbers[0] << endl;
+	cout << "Number at 2 is: " << Numbers[1] << endl;
+	cout << "Number beyond array: " << Numbers[5] << endl;
+	cout << "Elements in numbers array: " << std::size(Numbers) << endl;
+	cout << "Size of numbers array: " << sizeof(Numbers) << endl;
+
+	return 0;
+}
+
+*/
