2a9d5b77df4db42ef1716619eae72022e988547d
Mark Peters <mapeters101@earthlink.net>
2024-12-12 10:36:42 -0500

    Work on lesson067.

---
+++ lesson067.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+
+/*
+int main() {
+	int MArray[5][3];
+	MArray[0][0] = 1;
+	MArray[0][1] = 2;
+	MArray[0][2] = 3;
+	MArray[1][0] = 4;
+	MArray[1][1] = 5;
+	MArray[1][2] = 6;
+	MArray[2][0] = 7;
+	MArray[2][1] = 8;
+	MArray[2][2] = 9;
+	MArray[3][0] = 10;
+	MArray[3][1] = 11;
+	MArray[3][2] = 12;
+	MArray[4][0] = 13;
+	MArray[4][1] = 14;
+	MArray[4][2] = 15;
+
+	cout << MArray[3][2] << endl;
+
+	char MCharacter[3][4]{
+		{'A','B','C','D'},
+		{'E','F','G','H'},
+		{'I','J','K','L'} };
+
+	cout << MCharacter[1][2] << endl;
+	cout << MCharacter[0][2] << endl;
+
+	return 0;
+}
+
+*/
+
