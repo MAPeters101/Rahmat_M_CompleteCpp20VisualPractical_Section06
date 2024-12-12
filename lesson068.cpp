5069e6c198e1260aaaff1dcf053826d184aca21d
Mark Peters <mapeters101@earthlink.net>
2024-12-12 11:08:34 -0500

    Complete lesson068.

--- lesson068.cpp
+++ lesson068.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-
+/*
 int main() {
 	std::array<int, 5> MyArray{ 1, 3, 5, 7, 9 };
 	int NewArray[]{ 1, 3, 5, 7, 9 };
@@ -25,38 +25,52 @@ int main() {
 	//	cout << Num << endl;
 	//}
 
-	Values.fill(3.1415);
-	cout << "\nValues elements after fill: " << endl;
-	for (auto Num : Values) {
-		cout << Num << endl;
-	}
+	//Values.fill(3.1415);
+	//cout << "\nValues elements after fill: " << endl;
+	//for (auto Num : Values) {
+	//	cout << Num << endl;
+	//}
 
-	Values.at(0) = 4.3;
-	Values.at(1) = 5.2;
-	Values.at(2) = 2.9;
-	Values.at(3) = 0.4;
+	//Values.at(0) = 4.3;
+	//Values.at(1) = 5.2;
+	//Values.at(2) = 2.9;
+	//Values.at(3) = 0.4;
 
-	cout << "\nValues elements after after at(): " << endl;
-	cout << Values.at(0) << endl;
-	cout << Values.at(1) << endl;
-	cout << Values.at(2) << endl;
-	cout << Values.at(3) << endl;
-	//cout << Values.at(7) << endl;
+	//cout << "\nValues elements after after at(): " << endl;
+	//cout << Values.at(0) << endl;
+	//cout << Values.at(1) << endl;
+	//cout << Values.at(2) << endl;
+	//cout << Values.at(3) << endl;
+	////cout << Values.at(7) << endl;
 
-	cout << "\nValues elements after after at() using []: " << endl;
-	cout << Values[0] << endl;
-	cout << Values[1] << endl;
-	cout << Values[2] << endl;
-	cout << Values[3] << endl;
-	//cout << Values[7] << endl;
-
-
+	//cout << "\nValues elements after after at() using []: " << endl;
+	//cout << Values[0] << endl;
+	//cout << Values[1] << endl;
+	//cout << Values[2] << endl;
+	//cout << Values[3] << endl;
+	////cout << Values[7] << endl;
 
 
+	cout << "\nAll elements of Values Array before swap: " << endl;
+	for (auto Value : Values)
+		cout << Value << endl;
 
+	std::array<double, 4> Numbers{ 10.2, 10.9, 11.3, 12.9 };
+	cout << "\nAll elements of Numbers Array before swap: " << endl;
+	for (auto Number : Numbers)
+		cout << Number << endl;
 
+	Values.swap(Numbers);
+
+	cout << "\nAll elements of Values Array after swap: " << endl;
+	for (auto Value : Values)
+		cout << Value << endl;
+
+	cout << "\nAll elements of Numbers Array after swap: " << endl;
+	for (auto Number : Numbers)
+		cout << Number << endl;
 
 	return 0;
 }
-
+*/
 
