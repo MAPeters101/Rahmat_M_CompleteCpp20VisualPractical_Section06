0d2f9a900629210abe9b2164a142d7ff47c447e9
Mark Peters <mapeters101@earthlink.net>
2024-12-12 10:23:37 -0500

    Complete lesson066.

--- lesson066.cpp
+++ lesson066.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+/*
 int main() {
 	int Number[5];
 	Number[2] = 10;
@@ -17,8 +18,21 @@ int main() {
 	cout << "The first 3 elements in NewArray are: " << NewArray[0] << " " << NewArray[1] << " " << NewArray[2] << endl;
 	cout << "The last 3 elements in NewArray are: " << NewArray[3] << " " << NewArray[4] << " " << NewArray[5] << endl;
 	cout << "3 elements beyond NewArray are: " << NewArray[6] << " " << NewArray[7] << " " << NewArray[8] << endl;
+	cout << "--------------------------------------------\n\n";
+
+	int NewArray2[6]{ 1, 8 };
+	cout << "The first 3 elements in NewArray2 are: " << NewArray2[0] << " " << NewArray2[1] << " " << NewArray2[2] << endl;
+	cout << "The last 3 elements in NewArray2 are: " << NewArray2[3] << " " << NewArray2[4] << " " << NewArray2[5] << endl;
+	cout << "3 elements beyond NewArray2 are: " << NewArray2[6] << " " << NewArray2[7] << " " << NewArray2[8] << endl;
+	cout << "--------------------------------------------\n\n";
+
+	int NewArray3[]{ 1, 3, 5, 7, 9, 11 };
+	cout << "The first 3 elements in NewArray3 are: " << NewArray3[0] << " " << NewArray3[1] << " " << NewArray3[2] << endl;
+	cout << "The last 3 elements in NewArray3 are: " << NewArray3[3] << " " << NewArray3[4] << " " << NewArray3[5] << endl;
+	cout << "3 elements beyond NewArray3 are: " << NewArray3[6] << " " << NewArray3[7] << " " << NewArray3[8] << endl;
+	cout << "Memory location of the first element in NewArray3: " << NewArray3 << endl;
 
 	return 0;
 }
 
-
+*/
