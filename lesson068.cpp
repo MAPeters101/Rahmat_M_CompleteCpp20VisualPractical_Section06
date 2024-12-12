97169fde3e0f3eaeef7089ff159747bedcd7835f
Mark Peters <mapeters101@earthlink.net>
2024-12-12 11:01:14 -0500

    Work on lesson068 to 14:40.

--- lesson068.cpp
+++ lesson068.cpp
@@ -10,22 +10,50 @@ int main() {
 
 	std::array Values{ 1.2, 1.0, 3.1, 9.6 };
 
-	cout << "\nMyArray elements: " << endl;
-	for (int Num : MyArray) {
-		cout << Num << endl;
-	}
+	//cout << "\nMyArray elements: " << endl;
+	//for (int Num : MyArray) {
+	//	cout << Num << endl;
+	//}
 
-	cout << "\nNewArray elements: " << endl;
-	for (int Num : NewArray) {
-		cout << Num << endl;
-	}
+	//cout << "\nNewArray elements: " << endl;
+	//for (int Num : NewArray) {
+	//	cout << Num << endl;
+	//}
 
-	cout << "\nValues elements: " << endl;
+	//cout << "\nValues elements: " << endl;
+	//for (auto Num : Values) {
+	//	cout << Num << endl;
+	//}
+
+	Values.fill(3.1415);
+	cout << "\nValues elements after fill: " << endl;
 	for (auto Num : Values) {
 		cout << Num << endl;
 	}
 
-	
+	Values.at(0) = 4.3;
+	Values.at(1) = 5.2;
+	Values.at(2) = 2.9;
+	Values.at(3) = 0.4;
+
+	cout << "\nValues elements after after at(): " << endl;
+	cout << Values.at(0) << endl;
+	cout << Values.at(1) << endl;
+	cout << Values.at(2) << endl;
+	cout << Values.at(3) << endl;
+	//cout << Values.at(7) << endl;
+
+	cout << "\nValues elements after after at() using []: " << endl;
+	cout << Values[0] << endl;
+	cout << Values[1] << endl;
+	cout << Values[2] << endl;
+	cout << Values[3] << endl;
+	//cout << Values[7] << endl;
+
+
+
+
+
 
 
 	return 0;
